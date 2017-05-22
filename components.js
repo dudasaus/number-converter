f86f359170b622aa60b7e378c5e1406a095dc616
Austin Dudas <dudasaus@umich.edu>
2017-05-22 10:04:52 -0400

    Seperating components into new file

---
+++ components.js
@@ -0,0 +1,95 @@
+// components.js
+
+// requires and imports
+import React, { Component } from 'react';
+import {
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+// const values
+BINARY = 2, DECIMAL = 10, HEXADECIMAL = 16;
+const ss = require('./style.js');
+
+// number type selector buttons
+class NTSButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: false
+        }
+    }
+    render() {
+        let newStyle = this.props.style;
+        if (this.props.active) {
+            newStyle = newStyle.concat(ss.buttonActive);
+        }
+        return (
+            <Text style={newStyle}
+            onPress={this.props.onPress}>{this.props.name}</Text>
+        )
+    }
+}
+
+// number type selector
+class NumberTypeSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selection: BINARY
+        }
+    }
+
+    render() {
+        return (
+            <View style={ss.buttonContainer}>
+                <NTSButton
+                    style={[ss.buttonMiddle, ss.buttonFirst]}
+                    name="Binary"
+                    onPress={this.props.func(BINARY)}
+                    active={(this.props.type == BINARY)}
+                />
+                <NTSButton
+                    style={[ss.buttonMiddle]}
+                    name="Decimal"
+                    onPress={this.props.func(DECIMAL)}
+                    active={(this.props.type == DECIMAL)}
+
+                />
+                <NTSButton
+                    style={[ss.buttonMiddle, ss.buttonLast]}
+                    name="Hexadecimal"
+                    onPress={this.props.func(HEXADECIMAL)}
+                    active={(this.props.type == HEXADECIMAL)}
+                />
+            </View>
+        );
+    }
+}
+
+// number input
+class NumberInput extends Component {
+
+    render() {
+        return (
+            <TextInput
+                style={ss.textInput}
+                autoCapitalize='characters'
+                underlineColorAndroid='#35454D'
+                selectionColor='#B3E5FF'
+                onChangeText={this.props.func}
+                value={this.props.val.toString()}
+                keyboardType={(this.props.type==HEXADECIMAL)?'default':'numeric'}
+            />
+        );
+    }
+}
+
+module.exports = {
+    BINARY,
+    DECIMAL,
+    HEXADECIMAL,
+    NumberTypeSelector,
+    NumberInput
+}
--- index.android.js
+++ index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,79 +6,15 @@ import {
   View
 } from 'react-native';
 
+import {
+    NumberTypeSelector,
+    NumberInput,
+    BINARY,
+    DECIMAL,
+    HEXADECIMAL,
+} from './components.js';
+
 const ss = require('./style.js');
-const BINARY = 2, DECIMAL = 10, HEXADECIMAL = 16;
-
-class NTSButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false
-        }
-    }
-    render() {
-        let newStyle = this.props.style;
-        if (this.props.active) {
-            newStyle = newStyle.concat(ss.buttonActive);
-        }
-        return (
-            <Text style={newStyle}
-            onPress={this.props.onPress}>{this.props.name}</Text>
-        )
-    }
-}
-
-class NumberTypeSelector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selection: BINARY
-        }
-    }
-
-    render() {
-        return (
-            <View style={ss.buttonContainer}>
-                <NTSButton
-                    style={[ss.buttonMiddle, ss.buttonFirst]}
-                    name="Binary"
-                    onPress={this.props.func(BINARY)}
-                    active={(this.props.type == BINARY)}
-                />
-                <NTSButton
-                    style={[ss.buttonMiddle]}
-                    name="Decimal"
-                    onPress={this.props.func(DECIMAL)}
-                    active={(this.props.type == DECIMAL)}
-
-                />
-                <NTSButton
-                    style={[ss.buttonMiddle, ss.buttonLast]}
-                    name="Hexadecimal"
-                    onPress={this.props.func(HEXADECIMAL)}
-                    active={(this.props.type == HEXADECIMAL)}
-                />
-            </View>
-        );
-    }
-}
-
-class NumberInput extends Component {
-
-    render() {
-        return (
-            <TextInput
-                style={ss.textInput}
-                autoCapitalize='characters'
-                underlineColorAndroid='#35454D'
-                selectionColor='#B3E5FF'
-                onChangeText={this.props.func}
-                value={this.props.val.toString()}
-                keyboardType={(this.props.type==HEXADECIMAL)?'default':'numeric'}
-            />
-        );
-    }
-}
 
 class NumberConverter extends Component {
     // constructor
